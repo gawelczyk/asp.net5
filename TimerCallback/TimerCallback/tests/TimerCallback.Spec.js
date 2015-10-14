8da9684f52c233c3832dab0801d4def7c6e5d5e7
Adam Gawelczyk <adam@gawelczyk.com>
2015-10-14 16:34:18 +0200

    first version with view

--- TimerCallback/TimerCallback/tests/TimerCallback.Spec.js
+++ TimerCallback/TimerCallback/tests/TimerCallback.Spec.js
@@ -5,6 +5,7 @@
 
 /// <reference path="../wwwroot/lib/underscore.js" />
 /// <reference path="../wwwroot/lib/backbone.js" />
+/// <reference path="../wwwroot/lib/moment.js" />
 
 /// <reference path="../src/appjs/timercallback.js" />
 
@@ -35,14 +36,12 @@ describe("TimerCallback", function () {
     });
 
     describe("actions", function () {
-        var callback, secondsToCall = 10;
+        var callback, secondsToCall = 9;
 
         beforeEach(function () {
-            whatAmI = jasmine.createSpy('whatAmI');
             callback = jasmine.createSpy('aqq');
             app = new TimerCallbackApp(secondsToCall, callback);
             jasmine.clock().install();
-
         });
 
         afterEach(function () {
@@ -52,6 +51,13 @@ describe("TimerCallback", function () {
         it("timer is set", function () {
             expect(app.model.get('secondsToCall')).toBe(secondsToCall);
             expect(app.model.getMiliseconds()).toBe(secondsToCall * 1000);
+        });
+
+        it("endDate is set", function () {
+            console.log("getEndDate", app.model.getEndDate());
+            expect(app.model.getEndDate()).toBeTruthy();
+            expect(app.model.getEndDate().isSame(new Date().getTime() + secondsToCall * 1000, 'second')).toBe(true);
+            //expect(app.model.getMiliseconds()).toBe(secondsToCall * 1000);
         });
 
         it("callback is called synchronously after timer", function () {
@@ -69,6 +75,67 @@ describe("TimerCallback", function () {
             expect(callback).toHaveBeenCalled();
 
         });
+
+        it("calling start again blocks callback call and resets timer", function () {
+            app.start();
+
+            expect(callback).not.toHaveBeenCalled();
+
+            jasmine.clock().tick(secondsToCall * 500);
+            expect(callback).not.toHaveBeenCalled();
+
+            app.start();
+
+            jasmine.clock().tick(secondsToCall * 500);
+            expect(callback).not.toHaveBeenCalled();
+
+            jasmine.clock().tick(secondsToCall * 500 - 1);
+            expect(callback).not.toHaveBeenCalled();
+
+            jasmine.clock().tick(1);
+            expect(callback).toHaveBeenCalled();
+
+        });
+
+        it("callback called after set seconds", function () {
+            var t1, t2;
+            jasmine.clock().mockDate(new Date());
+            callback.and.callFake(function () {
+                t2 = new Date().getTime();
+            });
+
+            t1 = new Date().getTime();
+            app.start();
+
+            jasmine.clock().tick(secondsToCall * 1000);
+            expect(callback).toHaveBeenCalled();
+            expect(secondsToCall).toEqual((t2 - t1) / 1000);
+            console.log(t1, t2, 'diff:', t2 - t1, new Date().getTime() - t1);
+        });
+    });
+
+    describe("view", function () {
+        var callback, secondsToCall = 90,
+            domElement = '#timerInfo',
+            endDate = moment().add(secondsToCall, 's');
+
+        beforeEach(function () {
+            callback = jasmine.createSpy('aqq');
+            setFixtures("<div id='timerInfo'>...</div>");
+            app = new TimerCallbackApp(secondsToCall, callback, domElement);
+        });
+
+        it('view is set', function () {
+            expect(app.view).toBeTruthy();
+            expect(app.view.model).toBeTruthy();
+        });
+
+        it('view displays remining time', function () {
+            app.start();
+            expect($(domElement).text()).toBe(secondsToCall);
+            console.log('info', $(domElement), app.model.getEndDate(), $(domElement).text());
+        });
+
 
     });
 });
--- TimerCallback/TimerCallback/wwwroot/appJs/TimerCallback.js
+++ TimerCallback/TimerCallback/wwwroot/appJs/TimerCallback.js
@@ -1,31 +1,52 @@
 
 var TimerCallbackView = Backbone.View.extend({
-    initialize: function () { },
+    initialize: function () {
+        //var self = this;
+        //this.interval = setInterval(function () {
+        //    self.render();
+        //}, 500);
+    },
     events: {
     },
     render: function () {
+        this.$el.text(this.model.getEndDate().fromNow());
+        return this;
     }
 });
 
-var TimerCallbackModel = Backbone.Model.extend({    
+var TimerCallbackModel = Backbone.Model.extend({
+    initialize: function () {
+        this.endDate = moment().add(this.getMiliseconds(), 'ms');
+    },
     startTimer: function () {
+        if (this.timeout)
+            clearTimeout(this.timeout);
         this.timeout = setTimeout(this.get('callBack'), this.getMiliseconds());
         return this.timeout;
     },
     getMiliseconds: function () {
         return this.get('secondsToCall') * 1000;
+    },
+    getEndDate: function () {
+        return this.endDate;
     }
 });
 
-var TimerCallbackApp = function (secondsToCall, callBack) {
+var TimerCallbackApp = function (secondsToCall, callBack, element) {
 
     this.model = new TimerCallbackModel({
         secondsToCall: secondsToCall,
         callBack: callBack
     });
 
+    if (element) {
+        this.view = new TimerCallbackView({ model: this.model, el: element });
+    }
+
     this.start = function () {
         this.model.startTimer();
+        if (this.view)
+            this.view.render();
     };
 
 };
